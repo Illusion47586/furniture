7e4d7d3fc9ec056b2d641ddaefbd230e756fd639
illusion <dhruvtiwari110058@gmail.com>
2021-06-18 13:36:34 +0530

    navbar init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect, Fragment } from "react";
+
+import { motion, AnimatePresence } from "framer-motion";
+
+import { MagnifyingGlass, DotsThreeOutlineVertical, X } from "phosphor-react";
+import useWindowDimensions from "../hooks/windowDimensions";
+
+import classes from "../styles/css/components/navbar.module.css";
+
+const Navbar = () => {
+  const { width } = useWindowDimensions();
+  const [isExpanded, setIsExpanded] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setIsExpanded(!(width <= 768));
+    return () => {};
+  }, [width]);
+
+  return (
+    <Fragment>
+      <nav data-expanded={`${isExpanded}`}>
+        <h3 className={classes.no_margin}>Pineapple</h3>
+        {isExpanded && (
+          <ul className={classes.no_margin}>
+            <li>
+              <a href="/">About us</a>
+            </li>
+            <li>
+              <a href="/">Projects</a>
+            </li>
+            <li>
+              <a href="/">Services</a>
+            </li>
+            <li>
+              <a href="/">Shop</a>
+            </li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+          </ul>
+        )}
+        {isExpanded ? (
+          <MagnifyingGlass size={25} />
+        ) : isOpen ? (
+          <X size={25} onClick={() => setIsOpen(false)} />
+        ) : (
+          <DotsThreeOutlineVertical size={25} onClick={() => setIsOpen(true)} />
+        )}
+      </nav>
+      <AnimatePresence initial={false}>
+        {!isExpanded && isOpen && (
+          <motion.div
+            className={classes.menu}
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0 }}
+          >
+            <ul className={classes.no_margin}>
+              <motion.li
+                initial={{ x: -20, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                exit={{ x: 20, opacity: 0 }}
+              >
+                <a href="/">About us</a>
+              </motion.li>
+              <motion.li
+                initial={{ x: -20, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                exit={{ x: 20, opacity: 0 }}
+              >
+                <a href="/">Projects</a>
+              </motion.li>
+              <motion.li
+                initial={{ x: -20, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                exit={{ x: 20, opacity: 0 }}
+              >
+                <a href="/">Services</a>
+              </motion.li>
+              <motion.li
+                initial={{ x: -20, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                exit={{ x: 20, opacity: 0 }}
+              >
+                <a href="/">Shop</a>
+              </motion.li>
+              <motion.li
+                initial={{ x: -20, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                exit={{ x: 20, opacity: 0 }}
+              >
+                <a href="/">Contact</a>
+              </motion.li>
+            </ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </Fragment>
+  );
+};
+
+export default Navbar;
